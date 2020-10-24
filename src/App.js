a48488d147358c6e6c4b4c54537ca51d9bd8ef2c
ClubfootBear <ivan4bounty@gmail.com>
2020-10-24 10:07:35 +0500

    Lessons #13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+//Import components
+import Header from "./componets/Header";
+import NavBar from "./componets/NavBar";
+import Profile from "./componets/Profile";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <Header/>
+        <NavBar/>
+        <Profile/>
     </div>
   );
 }
---
+++ src/componets/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://million-wallpapers.ru/wallpapers/5/15/558809402572735.jpg" alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new posts
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                    <div>Post3</div>
+                    <div>Post4</div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
