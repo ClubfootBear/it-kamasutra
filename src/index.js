6c937acd774c42b4e68aa063b827b9398d82271b
ClubfootBear <ivan4bounty@gmail.com>
2020-10-26 05:23:43 +0500

    Lessond #38

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from "./redux/state";
-import {renderEntireTree} from "./renderEntireTree";
+import store from "./redux/state";
 
 
 
-renderEntireTree(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)}
+            />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,46 +1,58 @@
-import {renderEntireTree} from "../renderEntireTree";
-
-let state = {
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Vasay'},
-            {id: 2, name: 'Petya'},
-            {id: 3, name: 'Dima'},
-            {id: 4, name: 'Sveta'},
-        ],
-        dialogMessages: [
-            {id: 1, message: 'What did you say?'},
-            {id: 2, message: 'Hey!'},
-            {id: 3, message: 'WTF>>??'},
-        ],
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Vasay'},
+                {id: 2, name: 'Petya'},
+                {id: 3, name: 'Dima'},
+                {id: 4, name: 'Sveta'},
+            ],
+            dialogMessages: [
+                {id: 1, message: 'What did you say?'},
+                {id: 2, message: 'Hey!'},
+                {id: 3, message: 'WTF>>??'},
+            ],
+        },
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi', likesCount: 15,},
+                {id: 2, message: 'Da', likesCount: 35,},
+                {id: 3, message: 'Vot tak vot', likesCount: 5,},
+            ],
+            newPostText: 'It-Kamasutra',
+        },
+        sideBar: {},
     },
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi', likesCount: 15,},
-            {id: 2, message: 'Da', likesCount: 35,},
-            {id: 3, message: 'Vot tak vot', likesCount: 5,},
-        ],
-        newPostText: 'It-Kamasutra',
+
+    getState () {
+        return this._state
     },
-    sideBar: {},
 
-}
+    renderEntireTree() {
+        console.log('Privet')
+    },
 
-export let addPost = () => {
+    addPost() {
 
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        }
+        store._state.profilePage.posts.push(newPost)
+        store._state.profilePage.newPostText = '';
+        this.renderEntireTree(this._state)
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this.renderEntireTree(this._state)
+    },
+
+    subscribe(observer) {
+        this.renderEntireTree = observer;
     }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = '';
-    renderEntireTree(state)
+
 }
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state)
-}
-
-export default state;
+export default store;
